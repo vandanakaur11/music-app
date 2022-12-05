d2b24b4853473ab7938534a3e424c1831d84ae68
vandanakaur11 <vandana.softapps@gmail.com>
2022-12-05 19:09:50 +0500

    update services folder and next.config.js

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// console.log("process.env.base_url >>>>>>>>>", process.env.base_url);
-// console.log("process.env.media_url >>>>>>>>>", process.env.media_url);
+console.log("process.env.base_url >>>>>>>>>", process.env.base_url);
+console.log("process.env.media_url >>>>>>>>>", process.env.media_url);
 
 const api = axios.create({
   baseURL: process.env.base_url,
