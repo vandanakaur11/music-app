6445b397336a5c9d91f8e59a13dcde1140933c99
vandanakaur11 <vandana.softapps@gmail.com>
2022-12-05 20:30:04 +0500

    update next.config

--- next.config.js
+++ next.config.js
@@ -9,11 +9,14 @@ module.exports = (phase) => {
       images: {
         domains: [
           "localhost:5000",
+          // "musicfilesforheroku.s3.us-west-1.amazonaws.com",
           "musicfilesforianmulder.s3.us-west-1.amazonaws.com",
         ],
       },
       env: {
         base_url: "http://localhost:5000",
+        // media_url:
+        //   "https://musicfilesforheroku.s3.us-west-1.amazonaws.com/uploads",
         media_url:
           "https://musicfilesforianmulder.s3.us-west-1.amazonaws.com/uploads",
       },
@@ -31,13 +34,19 @@ module.exports = (phase) => {
     images: {
       domains: [
         "music-app-backend-alpha.vercel.app",
+        // "musicfilesforheroku.s3.us-west-1.amazonaws.com",
         "musicfilesforianmulder.s3.us-west-1.amazonaws.com",
       ],
     },
+    // images: { domains: ["localhost", "musicfilesforheroku.s3.us-west-1.amazonaws.com"] },
     env: {
       base_url: "https://music-app-backend-alpha.vercel.app",
+      // media_url:
+      //   "https://musicfilesforheroku.s3.us-west-1.amazonaws.com/uploads",
       media_url:
         "https://musicfilesforianmulder.s3.us-west-1.amazonaws.com/uploads",
+      // base_url: "https://music-appps.herokuapp.com/api",
+      // media_url: "https://music-appps.herokuapp.com",
       httpAgentOptions: {
         keepAlive: true,
       },
